d023c482f19644bfa4f4874601c48b10a2725678
chiakiiwasaki <chiaki103ki@gmail.com>
2021-08-03 17:28:44 +0000

    ベース機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :requests, only: [:index, :show, :edit, :update]
+    resources :posts, only: [:index, :show, :update]
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+  scope module: :public do
+    root 'homes#top'
+    get '/about' => 'homes#about'
+    get '/result' => 'homes#result'
+
+    resources :users, only: [:edit, :update]
+    get 'users/my_page' => 'users#show'
+    get 'users/unsubscribe' => 'users#unsubscribe'
+    patch 'users/withdraw' => 'users#withdraw'
+
+    resources :contacts, only: [:new, :create]
+    resources :rooms, only: [:create, :show]
+    resources :messages, only: [:create]
+    resources :requests, only: [:new, :create, :index, :show, :update]
+    resources :posts, only: [:new, :create, :index, :show]
+    # resources :post_comments, only: [:create, :destroy]
+    # resources :favorites, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
+  end
 end
