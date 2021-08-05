4cfec846d7f1e98bbb08e083f1bb3d45d74d747e
chiakiiwasaki <chiaki103ki@gmail.com>
2021-08-05 11:38:07 +0000

    退会機能実装

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = current_user
   end
@@ -11,19 +13,22 @@ class Public::UsersController < ApplicationController
     @user = current_user
     if @user.update(user_params)
       redirect_to users_my_page_path
-    else
-      render 'edit'
     end
   end
 
   def unsubscribe
+    @user = current_user
   end
 
   def withdraw
+    @user = current_user
+    @user.update(is_valid: false)
+    reset_session
+    redirect_to root_path
   end
 
   private
     def user_params
-      params.require(:user).permit(:image_id, :name, :email, :introduction)
+      params.require(:user).permit(:image, :name, :email, :introduction)
     end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  before_action :reject_inactive_user, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +25,11 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def reject_inactive_user
+    @user = User.find_by(email: params[:user][:email])
+    if @user.valid_password?(params[:user][:password]) && !@user.is_valid
+      flash[:alert] = "このアカウントは退会済みです"
+      redirect_to new_user_session_path
+    end
+  end
 end
