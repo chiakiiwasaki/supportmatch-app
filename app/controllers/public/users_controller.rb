cf997882dd110b16ed6e587f55bb6cba75719242
chiakiiwasaki <chiaki103ki@gmail.com>
2021-08-05 08:17:24 +0000

    Initial Commit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,9 +3,16 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.save(user_params)
+      redirect_to users_my_page_path
+    else
+      render 'edit'
+    end
   end
 
   def unsubscribe
@@ -13,4 +20,9 @@ class Public::UsersController < ApplicationController
 
   def withdraw
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:image_id, :name, :email, :introduction)
+    end
 end
