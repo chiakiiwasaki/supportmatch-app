9bcd5180f98cb9325213571ad690b1fc5158cde6
chiakiiwasaki <chiaki103ki@gmail.com>
2021-08-15 09:32:10 +0000

    コメント非同期

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -7,14 +7,14 @@ class Public::PostCommentsController < ApplicationController
     @post_comment.user_id = current_user.id
     @post_comment.post_id = @post.id
     @post_comment.save
-    redirect_to post_path(@post)
+    # redirect_to post_path(@post)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     post_comment = @post.post_comments.find(params[:id])
     post_comment.destroy
-    redirect_to post_path(@post)
+    # redirect_to post_path(@post)
   end
 
   private
