f4aa722136fe9bdad855606f420c3ce9b9256f77
chiakiiwasaki <chiaki103ki@gmail.com>
2021-08-27 18:36:22 +0000

    レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
-    passwords: 'admins/passwords',
-    registrations: 'admins/registrations'
+    # passwords: 'admins/passwords',
+    # registrations: 'admins/registrations'
   }
 
   namespace :admins do
--- db/migrate/20210805061502_add_introduction_to_users.rb
+++ db/migrate/20210805061502_add_introduction_to_users.rb
@@ -3,6 +3,7 @@ class AddIntroductionToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :introduction, :text
     add_column :users, :image_id, :string
     add_column :users, :is_valid, :boolean, default: true, null: false
+    
 
   end
 end
